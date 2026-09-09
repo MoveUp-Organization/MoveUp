f1c495ddd7b0d52029d378e7e68dbd5d818809a2
marifernandesgm <marifernandesgm17@gmail.com>
2026-09-09 13:15:59 -0300

    feat: Arrumando os componentes Cabecalho, Botao e Rodape em suas devidas pastas.

---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,8 @@
+export default function Cabecalho(){
+
+    return (
+        <header>
+            <h1>MoveUP</h1>
+        </header>
+    );
+}
---
+++ src/components/Rodape/Rodape.tsx
@@ -0,0 +1,8 @@
+export default function Rodape(){
+
+    return (
+        <footer>
+            <p>&copy; 2026 - Todos os direitos reservados - Boiler Plate</p>
+        </footer>
+    );
+}
