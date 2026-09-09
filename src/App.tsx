9ff828b0a616e332b80a088af41e75f087876786
isabelleferreira <isabelleferreiraa999@gmail.com>
2026-09-09 19:45:04 -0300

    chore: configurando router e tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
 
